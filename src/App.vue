77da92df5ba78c08cede5da44e5c425e64a6903f
YangYi <CHNyangyi@petalmail.com>
2022-03-19 01:41:54 +0800

    [前端搭建]: 修复头部菜单不固定和不是最优先的问题

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <template>
   <div id="app">
+    <Header class="header" />
     <router-view/>
   </div>
 </template>
+
+<script>
+
+  import Header from "./components/Header";
+
+  export default {
+    name: "App",
+    components: {
+      Header
+    }
+  }
+</script>
 
 <style>
 #app {
 
 }
+  .header {
+    position: fixed !important;
+    left: 0;
+    top: 0;
+    z-index: 1000;
+    height: 64px;
+    width: 100%;
+    padding: 0 24px;
+    background: #FFFFFF;
+    box-shadow: 6px 0 2px rgba(0 21 41 / 35%);
+  }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,7 @@
 <template>
   <div>
-    <el-container style="padding: 0 0">
-      <el-header style="height: 64px; padding: 0 24px" class="bs-v">
-        <Header style="height: 64px" />
-      </el-header>
-
-      <el-main style="padding: 0 0px">
-        <Main />
-        <Footer />
-      </el-main>
-    </el-container>
+    <Main />
+    <Footer />
   </div>
 </template>
 
